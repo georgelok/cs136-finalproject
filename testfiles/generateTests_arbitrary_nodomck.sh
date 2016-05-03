f8c9276778ebc476937acde8dbcc413dc547cdf1
George Lok <george.j.lok@gmail.com>
2016-05-03 00:53:02 -0400

    more test files

--- testfiles/generateTests_arbitrary_nodomck.sh
+++ testfiles/generateTests_arbitrary_nodomck.sh
@@ -3,6 +3,6 @@ for i in 100 500 1000
 do
 	for j in 100 500 1000 2000 4000
 	do
-		./cats -d arbitrary -goods $i -bids $j -filename 3atom/3atom-$i-$j- -n 5 -cplex -no_dom_check
+		./cats -d arbitrary -goods $i -bids $j -filename arbitrary_nodomchk/arbitrary-$i-$j- -n 5 -cplex -no_dom_check
 	done
 done
